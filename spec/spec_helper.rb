f795f98680976d2f047dd85664183adebcf07836
Dave Shawley <daveshawley@gmail.com>
2014-01-05 21:48:01 -0500

    spec_helper: Use ChefSpec 3 Berkshelf integration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # Pull in commonly used libraries.
 require 'chefspec'
+require 'chefspec/berkshelf'
 require 'support/matchers.rb'
 
 # Configure SafeYAML so that it doesn't complain so much.
@@ -10,40 +11,3 @@ SafeYAML::OPTIONS[:default_mode] = :safe
 
 # This is a workaround to https://github.com/dtao/safe_yaml/issues/10
 SafeYAML::OPTIONS[:deserialize_symbols] = true
-
-# ChefSpec sets up the Chef cookbook_path so that it includes the
-# vendor/cookbooks directory.  We need to tell Berkshelf to install
-# our dependencies there.
-require 'berkshelf'
-berks = ::Berkshelf::Berksfile.from_file 'Berksfile'
-RSpec.configure do |config|
-  config.cookbook_path = 'vendor/cookbooks'
-  config.platform = 'ubuntu'
-  config.version = '12.04'
-  config.before(:suite) do
-    FileUtils.rm_rf 'vendor/cookbooks'
-    berks.install path: 'vendor/cookbooks'
-  end
-  config.after(:suite) do
-    FileUtils.rm_rf 'vendor/cookbooks'
-  end
-end
-
-# Dump the resources associated with a ChefSpec::Runner.
-#
-# This is best used in a `subject` block:
-#
-#     subject {
-#       dump_chef_run @chef_run.converge described_recipe
-#     }
-#
-# This will output a list of resources that participated in the run so that
-# you can easily see which matchers should have been generated.
-#
-def dump_chef_run(runner)
-  runner.run_context.resource_collection.each do |elm|
-    puts "... #{elm.action}_#{elm.resource_name} '#{elm.name}'"
-    yield(elm) if block_given?
-  end
-  runner
-end
